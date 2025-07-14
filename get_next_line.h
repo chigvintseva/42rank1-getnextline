c1029e67e22aaec822bb261fdc0522c4e78ec79e
alena <141341998+chigvintseva@users.noreply.github.com>
2025-07-15 01:31:06 +0200

    prototypes of all task-specific functions to be used

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,17 @@
-#defif GET_NEXT_LINE_H
 #ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
 char	*get_next_line(int fd);
+char	*ft_read(int fd, char *buffer);
+char	*ft_update_buffer(char *chunk, char *buffer);
+char	*ft_extract_line(char *buffer);
+
 
 # endif
