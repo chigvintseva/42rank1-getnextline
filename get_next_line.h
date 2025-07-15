47acf947c1a0fa55b5595cff1f62b8cfef5aaac1
alena <141341998+chigvintseva@users.noreply.github.com>
2025-07-15 13:41:15 +0200

    helper functions form libft

--- get_next_line.h
+++ get_next_line.h
@@ -13,6 +13,8 @@ char	*get_next_line(int fd);
 char	*ft_read(int fd, char *buffer);
 char	*ft_update_buffer(char *chunk, char *buffer);
 char	*ft_extract_line(char *buffer);
-
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 # endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,7 +1,45 @@
 #include "get_next_line.h"
 
-ft_strchr
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
 
-ft_strjoin
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 
-ft_strlen
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	chr;
+
+	chr = (unsigned char)c;
+	while (*s && (*s != chr))
+		s++;
+	if (*s == chr)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i] != 0)
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	return (str);
+}
